b77cefcdef1141641c67062c58d6970758f38d4d
Umananda Siddha <umanandasiddha243@gmail.com>
2024-09-04 21:55:00 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import GsapFrom from "./pages/GsapFrom";
+import GsapFromTo from "./pages/GsapFromTo";
+import GsapScrollTrigger from "./pages/GsapScrollTrigger";
+import GsapStagger from "./pages/GsapStagger";
+import GsapText from "./pages/GsapText";
+import GsapTimeline from "./pages/GsapTimeline";
+import GsapTo from "./pages/GsapTo";
+
+const App = () => {
+    return (
+        <div>
+            {/* <GsapTo /> */}
+            {/* <GsapFrom /> */}
+            {/* <GsapFromTo /> */}
+            {/* <GsapTimeline /> */}
+            {/* <GsapStagger /> */}
+            {/* <GsapScrollTrigger /> */}
+            <GsapText />
+        </div>
+    )
+}
+
+export default App;
